ea67781d32286420ee80d572589cebeb81519b6e
kristaps <>
2014-04-07 13:02:48 +0000

    Framework -> library.

--- kcgi-local.h
+++ kcgi-local.h
@@ -1,18 +1,6 @@
 /*	$Id$ */
 #ifndef KCGI_LOCAL_H
 #define KCGI_LOCAL_H
-
-enum	page {
-	PAGE_INDEX,
-	PAGE__MAX
-};
-
-enum	key {
-	KEY_SESSID, 
-	KEY_SESSCOOKIE, 
-	KEY__MAX
-};
-
 __BEGIN_DECLS
 
 __END_DECLS
