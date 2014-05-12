031df661136fb07c9c34673e1af413a08f6b6110
kristaps <>
2014-05-12 15:43:26 +0000

    Integrate sandbox model.

--- extern.h
+++ extern.h
@@ -1,10 +1,28 @@
+/*	$Id$ */
+/*
+ * Copyright (c) 2014 Kristaps Dzonsons <kristaps@bsd.lv>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
 #ifndef EXTERN_H
 #define EXTERN_H
 
 __BEGIN_DECLS
 
 int	 khttp_input_parent(int fd, struct kreq *r, pid_t pid);
-void	 khttp_input_child(void);
+void	 khttp_input_child(int fd);
+
+int	 ksandbox_init(void);
 
 __END_DECLS
 
