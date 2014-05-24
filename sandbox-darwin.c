b30b298275bd324354803d1559545bbc4b065fc0
kristaps <>
2014-05-24 13:02:15 +0000

    Split out sandbox code.

---
+++ sandbox-darwin.c
@@ -0,0 +1,71 @@
+/*	$Id$ */
+/*
+ * Copyright (c) 2012, 2014 Kristaps Dzonsons <kristaps@bsd.lv>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#ifdef HAVE_SANDBOX_INIT
+
+#include <sys/resource.h>
+
+#include <sandbox.h>
+#include <stdarg.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#include "kcgi.h"
+#include "extern.h"
+
+int
+ksandbox_darwin_init_child(void *arg)
+{
+	int	 	 rc;
+	char		*er;
+	struct rlimit	 rl_zero;
+
+	rl_zero.rlim_cur = rl_zero.rlim_max = 0;
+
+	if (-1 == setrlimit(RLIMIT_FSIZE, &rl_zero))
+		XWARN("setrlimit: rlimit_fsize");
+#if 0
+	/*
+	 * FIXME: I've taken out the RLIMIT_NOFILE setrlimit() because
+	 * it causes strange behaviour.  On Mac OS X, it fails with
+	 * EPERM no matter what (the same code runs fine when not run as
+	 * a CGI instance).
+	 */
+	if (-1 == setrlimit(RLIMIT_NOFILE, &rl_zero))
+		XWARN("setrlimit: rlimit_fsize");
+#endif
+	if (-1 == setrlimit(RLIMIT_NPROC, &rl_zero))
+		XWARN("setrlimit: rlimit_nproc");
+
+	rc = sandbox_init
+		(kSBXProfilePureComputation, 
+		 SANDBOX_NAMED, &er);
+
+	if (0 == rc)
+		return(1);
+
+	XWARNX("sandbox_init: %s", er);
+	sandbox_free_error(er);
+	return(0);
+}
+
+#else
+int dummy;
+#endif
