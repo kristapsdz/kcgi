08e6509f1b7379d09c875488395b1b10b540497b
kristaps <>
2015-02-14 12:29:54 +0000

    Upgrade AFL infrastructure to use dictionaries and per-content-type.

---
+++ afl/afl-multipart.c
@@ -0,0 +1,72 @@
+/*	$Id$ */
+/*
+ * Copyright (c) 2015 Kristaps Dzonsons <kristaps@bsd.lv>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+#ifdef HAVE_CONFIG_H
+#include "../config.h"
+#endif
+#include <sys/stat.h>
+
+#include <fcntl.h>
+#include <paths.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "../kcgi.h"
+#include "../extern.h"
+
+int
+main(int argc, char *argv[])
+{
+	int	 	 fdout, fdin;
+	struct kworker	 work;
+	struct stat	 st;
+	char		 buf[1024];
+
+	if (2 != argc)
+		return(EXIT_FAILURE);
+
+	if (-1 == (fdout = open(_PATH_DEVNULL, O_RDWR, 0))) {
+		perror(_PATH_DEVNULL);
+		return(EXIT_FAILURE);
+	} else if (-1 == (fdin = open(argv[1], O_RDONLY, 0))) {
+		perror(argv[1]);
+		close(fdout);
+		return(EXIT_FAILURE);
+	} else if (-1 == fstat(fdin, &st)) {
+		perror(argv[1]);
+		close(fdout);
+		close(fdin);
+		return(EXIT_FAILURE);
+	}
+
+	snprintf(buf, sizeof(buf), "%llu", 
+		(unsigned long long)st.st_size);
+	setenv("CONTENT_TYPE", "multipart/form-data; "
+		"boundary=---------------------------9051914041544843365972754266", 1);
+	setenv("REQUEST_METHOD", "post", 1);
+	setenv("CONTENT_LENGTH", buf, 1);
+	memset(&work, 0, sizeof(struct kworker));
+	work.sock[KWORKER_WRITE] = fdout;
+	work.sock[KWORKER_READ] = -1;
+	work.input = fdin;
+	khttp_input_child(&work, NULL, 0, kmimetypes, KMIME__MAX);
+	close(fdin);
+	close(fdout);
+	return(EXIT_SUCCESS);
+}
