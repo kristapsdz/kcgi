5e41927bafe0cf24f3114328ed500682377deaf0
kristapsdz <kristaps@bsd.lv>
2015-03-01 23:06:44 +0000

    Compile on OpenBSD.

--- kcgiregress.c
+++ kcgiregress.c
@@ -24,6 +24,7 @@
 #include <errno.h>
 #include <fcntl.h>
 #include <stdio.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include <signal.h>
 #include <string.h>
--- master.c
+++ master.c
@@ -28,6 +28,7 @@
 #include <inttypes.h>
 #include <fcntl.h>
 #include <signal.h>
+#include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
