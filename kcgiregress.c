927f1d62d0b143b3367d239734ccbd2356f228d2
kristapsdz <kristaps@bsd.lv>
2015-08-01 17:51:38 +0000

    Version notes and some (unfinished) work on the regression suite.

--- kcgiregress.c
+++ kcgiregress.c
@@ -36,19 +36,13 @@
 #include "kcgiregress.h"
 
 static int
-dochild_cgi(kcgi_regress_server child, void *carg)
+dochild_prepare(void)
 {
-	int	 	 s, in, rc, opt, first;
+	int	 	 s, in, opt;
 	struct sockaddr_in ad, rem;
 	socklen_t	 len;
-	char		 head[1024], headbuf[1024];
-	char		*cp, *path, *query, *key, *val;
-	size_t		 sz;
-	extern char	*__progname;
 
-	rc = 0;
 	opt = 1;
-	in = -1;
 	memset(&ad, 0, sizeof(struct sockaddr_in));
 
 	/*
@@ -57,11 +51,11 @@ dochild_cgi(kcgi_regress_server child, void *carg)
 	 */
 	if (-1 == (s = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP))) {
 		perror("socket");
-		exit(EXIT_FAILURE);
+		return(-1);
 	} else if (-1 == setsockopt(s, SOL_SOCKET, 
 		 SO_REUSEADDR, &opt, sizeof(opt))) {
 		perror("setsockopt");
-		goto out;
+		return(-1);
 	}
 
 	ad.sin_family = AF_INET;
@@ -70,10 +64,12 @@ dochild_cgi(kcgi_regress_server child, void *carg)
 
 	if (-1 == bind(s, (struct sockaddr *)&ad, sizeof(ad))) {
 		perror("bind");
-		goto out;
+		close(s);
+		return(-1);
 	} else if (-1 == listen(s, 1)) {
 		perror("listen");
-		goto out;
+		close(s);
+		return(-1);
 	}
 
 	/*
@@ -90,8 +86,154 @@ dochild_cgi(kcgi_regress_server child, void *carg)
 	len = sizeof(struct sockaddr_in);
 	if (-1 == (in = accept(s, (struct sockaddr *)&rem, &len))) {
 		perror("accept");
-		goto out;
+		close(s);
+		return(-1);
 	} 
+
+	close(s);
+	return(in);
+}
+
+#if 0
+static int
+dochild_fcgi(kcgi_regress_server child, void *carg)
+{
+	int	 	 in, rc, first;
+	char		 head[1024], headbuf[1024];
+	char		*cp, *path, *query, *key, *val;
+	size_t		 sz;
+	pid_t		 pid;
+	extern char	*__progname;
+
+	/*
+	 * Begin by forking the FastCGI process.
+	 * We need to use a separate process because we're going to
+	 * package the request as a FastCGI request and ship it into the
+	 * UNIX socket.
+	 */
+	if (-1 == (pid = fork())) {
+		perror("fork");
+		goto out;
+	} else if (0 == pid)
+		return(child(carg));
+
+	rc = 0;
+	if (-1 == (in = dochild_prepare())) 
+		return(0);
+
+	setenv("SCRIPT_NAME", __progname, 1);
+	
+	/*
+	 * Read header lines.
+	 * We'll do this in the simplest possible way so that there are
+	 * no surprises.
+	 */
+	first = 1;
+	while (NULL != fgets(head, sizeof(head), stdin)) {
+		/* Strip off the CRLF terminator. */
+		if ((sz = strlen(head)) < 2) {
+			fprintf(stderr, "Bad HTTP header\n");
+			goto out;
+		}
+		if ('\n' != head[sz - 1] || '\r' != head[sz - 2]) {
+			fprintf(stderr, "Unterminated HTTP header\n");
+			goto out;
+		}
+		head[sz - 2] = '\0';
+
+		/* Empty line: now we're at the CGI document. */
+		if ('\0' == head[0])
+			break;
+
+		/* Process our header. */
+		if (first) {
+			/* Snarf the first GET/POST line. */
+			if (0 == strncmp(head, "GET ", 4)) {
+				setenv("REQUEST_METHOD", "GET", 1);
+				path = head + 4;
+			} else if (0 == strncmp(head, "POST ", 5)) {
+				setenv("REQUEST_METHOD", "POST", 1);
+				path = head + 5;
+			} else {
+				fprintf(stderr, "Unknown HTTP "
+					"first line: %s\n", head);
+				goto out;
+			}
+
+			/* Split this into the path and query. */
+			cp = path;
+			while ('\0' != *cp && ! isspace(*cp))
+				cp++;
+			*cp = '\0';
+			if (NULL != (query = strchr(path, '?')))
+				*query++ = '\0';
+
+			first = 0;
+			setenv("PATH_INFO", path, 1);
+			if (NULL != query)
+				setenv("QUERY_STRING", query, 1);
+			continue;
+		}
+
+		/* 
+		 * Split headers into key/value parts.
+		 * Strip the leading spaces on the latter. 
+		 * Let baddies (no value) just go by.
+		 */
+		key = head;
+		if (NULL == (val = strchr(key, ':')))
+			continue;
+		*val++ = '\0';
+		while ('\0' != *val && isspace(*val))
+			val++;
+
+		/* Recognise some attributes... */
+		if (0 == strcmp(key, "Content-Length")) {
+			setenv("CONTENT_LENGTH", val, 1);
+			continue;
+		} else if (0 == strcmp(key, "Content-Type")) {
+			setenv("CONTENT_TYPE", val, 1);
+			continue;
+		}
+
+		/*
+		 * Now we have "regular" attributes that we want to cast
+		 * directly as HTTP attributes in the CGI environment.
+		 */
+		strlcpy(headbuf, "HTTP_", sizeof(headbuf));
+		sz = strlcat(headbuf, key, sizeof(headbuf));
+		assert(sz < sizeof(headbuf));
+		for (cp = headbuf; '\0' != *cp; cp++) 
+			if ('-' == *cp)
+				*cp = '_';
+			else if (isalpha((int)*cp))
+				*cp = toupper((int)*cp);
+
+		setenv(headbuf, val, 1);
+	}
+
+	if (-1 != in)
+		close(in);
+	return(child(carg));
+out:
+	if (-1 != in)
+		close(in);
+	return(rc);
+}
+#endif
+
+static int
+dochild_cgi(kcgi_regress_server child, void *carg)
+{
+	int	 	 in, rc, first;
+	char		 head[1024], headbuf[1024];
+	char		*cp, *path, *query, *key, *val;
+	size_t		 sz;
+	extern char	*__progname;
+
+	rc = 0;
+	if (-1 == (in = dochild_prepare())) 
+		return(0);
 
 	/*
 	 * Assign the socket as our stdin and stdout.
@@ -197,16 +339,10 @@ dochild_cgi(kcgi_regress_server child, void *carg)
 		setenv(headbuf, val, 1);
 	}
 
-	if (-1 != in)
-		close(in);
-	if (-1 != s)
-		close(s);
+	close(in);
 	return(child(carg));
 out:
-	if (-1 != in)
-		close(in);
-	if (-1 != s)
-		close(s);
+	close(in);
 	return(rc);
 }
 
