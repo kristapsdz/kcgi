20067f08141e32456fe61d35750a48f2d64f0ef8
kristaps <>
2015-08-05 21:31:48 +0000

    Add Linux sandbox for control.

--- sandbox-seccomp-filter.c
+++ sandbox-seccomp-filter.c
@@ -78,8 +78,57 @@
 	BPF_JUMP(BPF_JMP+BPF_JEQ+BPF_K, __NR_ ## _nr, 0, 1), \
 	BPF_STMT(BPF_RET+BPF_K, SECCOMP_RET_ALLOW)
 
+static const struct sock_filter preauth_ctrl[] = {
+	/* Ensure the syscall arch convention is as expected. */
+	BPF_STMT(BPF_LD+BPF_W+BPF_ABS,
+		offsetof(struct seccomp_data, arch)),
+	BPF_JUMP(BPF_JMP+BPF_JEQ+BPF_K, SECCOMP_AUDIT_ARCH, 1, 0),
+	BPF_STMT(BPF_RET+BPF_K, SECCOMP_FILTER_FAIL),
+	/* Load the syscall number for checking. */
+	BPF_STMT(BPF_LD+BPF_W+BPF_ABS,
+		offsetof(struct seccomp_data, nr)),
+	SC_DENY(open, EACCES),
+	SC_ALLOW(getpid),
+	SC_ALLOW(gettimeofday),
+	SC_ALLOW(clock_gettime),
+#ifdef __NR_time /* not defined on EABI ARM */
+	SC_ALLOW(time),
+#endif
+	SC_ALLOW(accept),
+	SC_ALLOW(fcntl),
+	SC_ALLOW(sendmsg),
+	SC_ALLOW(read),
+	SC_ALLOW(write),
+	SC_ALLOW(close),
+#ifdef __NR_shutdown /* not defined on archs that go via socketcall(2) */
+	SC_ALLOW(shutdown),
+#endif
+	SC_ALLOW(brk),
+	SC_ALLOW(poll),
+#ifdef __NR__newselect
+	SC_ALLOW(_newselect),
+#else
+	SC_ALLOW(select),
+#endif
+	SC_ALLOW(madvise),
+#ifdef __NR_mmap2 /* EABI ARM only has mmap2() */
+	SC_ALLOW(mmap2),
+#endif
+#ifdef __NR_mmap
+	SC_ALLOW(mmap),
+#endif
+	SC_ALLOW(munmap),
+	SC_ALLOW(exit_group),
+#ifdef __NR_rt_sigprocmask
+	SC_ALLOW(rt_sigprocmask),
+#else
+	SC_ALLOW(sigprocmask),
+#endif
+	BPF_STMT(BPF_RET+BPF_K, SECCOMP_FILTER_FAIL),
+};
+
 /* Syscall filtering set for preauth. */
-static const struct sock_filter preauth_insns[] = {
+static const struct sock_filter preauth_work[] = {
 	/* Ensure the syscall arch convention is as expected. */
 	BPF_STMT(BPF_LD+BPF_W+BPF_ABS,
 		offsetof(struct seccomp_data, arch)),
@@ -126,9 +175,14 @@ static const struct sock_filter preauth_insns[] = {
 	BPF_STMT(BPF_RET+BPF_K, SECCOMP_FILTER_FAIL),
 };
 
-static const struct sock_fprog preauth_program = {
-	.len = (unsigned short)(sizeof(preauth_insns)/sizeof(preauth_insns[0])),
-	.filter = (struct sock_filter *)preauth_insns,
+static const struct sock_fprog preauth_prog_work = {
+	.len = (unsigned short)(sizeof(preauth_work)/sizeof(preauth_work[0])),
+	.filter = (struct sock_filter *)preauth_work,
+};
+
+static const struct sock_fprog preauth_prog_ctrl = {
+	.len = (unsigned short)(sizeof(preauth_ctrl)/sizeof(preauth_ctrl[0])),
+	.filter = (struct sock_filter *)preauth_ctrl,
 };
 
 #ifdef SANDBOX_SECCOMP_DEBUG
@@ -198,7 +252,7 @@ ksandbox_seccomp_init_child(void *arg, enum sandtype type)
 		XWARN("prctl(PR_SET_NO_NEW_PRIVS)");
 		nnp_failed = 1;
 	}
-	if (prctl(PR_SET_SECCOMP, SECCOMP_MODE_FILTER, &preauth_program) == -1)
+	if (prctl(PR_SET_SECCOMP, SECCOMP_MODE_FILTER, &preauth_prog_work) == -1)
 		XWARN("prctl(PR_SET_SECCOMP)");
 	else if (nnp_failed) {
 		XWARNX("SECCOMP_MODE_FILTER activated but "
