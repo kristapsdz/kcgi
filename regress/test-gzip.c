3201b74fadea8dddc03bc348bdd621bc537cbb8f
kristaps <>
2016-03-10 01:54:30 +0000

    Get compiling on Alpine Linux.  Motivated by Vitaliy T.--thanks!

--- regress/test-gzip.c
+++ regress/test-gzip.c
@@ -14,7 +14,9 @@
  * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
+#ifdef HAVE_CONFIG_H
 #include "../config.h"
+#endif
 
 #include <stdint.h>
 #include <stdlib.h>
