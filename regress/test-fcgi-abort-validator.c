ea98be6828e2c4ddf5e7722904dde8f663c8fa42
kristaps <>
2016-03-15 03:50:16 +0000

    Add an abort validator test for FastCGI. This flushed out a bunch of bugs.

---
+++ regress/test-fcgi-abort-validator.c
@@ -0,0 +1,75 @@
+/*	$Id$ */
+/*
+ * Copyright (c) 2015 Kristaps Dzonsons <kristaps@bsd.lv>
+ *
+ * Permission to use, copy, modify, and distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+#ifdef HAVE_CONFIG_H
+#include "../config.h"
+#endif
+
+#include <stdint.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <curl/curl.h>
+
+#include "../kcgi.h"
+#include "regress.h"
+
+static int
+parent(CURL *curl)
+{
+
+	curl_easy_setopt(curl, CURLOPT_URL, 
+		"http://localhost:17123/index.html?foo=bar");
+	return(CURLE_GOT_NOTHING == curl_easy_perform(curl));
+}
+
+static int
+kvalid_abort(struct kpair *kp)
+{
+
+	abort();
+	return(0);
+}
+
+static int
+child(void)
+{
+	struct kreq	 r;
+	const char 	*page = "index";
+	struct kfcgi	*fcgi;
+	enum kcgi_err	 er;
+	struct kvalid	 key = { kvalid_abort, "foo" };
+
+	if ( ! khttp_fcgi_test())
+		return(0);
+
+	if (KCGI_OK != khttp_fcgi_init(&fcgi, &key, 1, &page, 1, 0))
+		return(0);
+
+	if (KCGI_FORM != khttp_fcgi_parse(fcgi, &r)) {
+		khttp_free(&r);
+		return(0);
+	}
+	khttp_fcgi_free(fcgi);
+	return(1);
+}
+
+int
+main(int argc, char *argv[])
+{
+
+	return(regress_fcgi(parent, child) ? EXIT_SUCCESS : EXIT_FAILURE);
+}
