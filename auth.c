ede8f839fa8672f3184969f07cdeb63c990e76b8
Jamil Navarro <jamilnavarro@gmail.com>
2016-10-31 12:08:50 -0300

    Fixed warning: "auth.c:172:3: warning: format ‘%lx’ expects argument of type ‘long unsigned int’, but argument 4 has type ‘size_t’ [-Wformat=]"

--- auth.c
+++ auth.c
@@ -169,7 +169,7 @@ khttpdigest_validatehash(const struct kreq *req, const char *skey4)
 
 	if (KHTTPQOP_AUTH_INT == auth->qop || 
 	    KHTTPQOP_AUTH == auth->qop) {
-		snprintf(count, sizeof(count), "%08lx", auth->count);
+		snprintf(count, sizeof(count), "%08zx", auth->count);
 		MD5Init(&ctx);
 		MD5Update(&ctx, skey1, MD5_DIGEST_LENGTH * 2);
 		MD5Update(&ctx, ":", 1);
