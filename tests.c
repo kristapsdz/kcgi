7c371589ea35f2947da583d117908517be8332a0
kristaps <>
2019-07-05 12:22:40 +0000

    Bring up to date with latest oconfigure.

--- tests.c
+++ tests.c
@@ -377,8 +377,18 @@ int
 main(void)
 {
 	struct fooq foo_q;
+	struct foo *p, *tmp;
+	int i = 0;
 
 	TAILQ_INIT(&foo_q);
+
+	/*
+	 * Use TAILQ_FOREACH_SAFE because some systems (e.g., Linux)
+	 * have TAILQ_FOREACH but not the safe variant.
+	 */
+
+	TAILQ_FOREACH_SAFE(p, &foo_q, entries, tmp)
+		p->bar = i++;
 	return 0;
 }
 #endif /* TEST_SYS_QUEUE */
@@ -395,6 +405,15 @@ main(void)
 	return(0);
 }
 #endif /* TEST_SYSTRACE */
+#if TEST_UNVEIL
+#include <unistd.h>
+
+int
+main(void)
+{
+	return -1 != unveil(NULL, NULL);
+}
+#endif /* TEST_UNVEIL */
 #if TEST_ZLIB
 #include <stddef.h>
 #include <zlib.h>
