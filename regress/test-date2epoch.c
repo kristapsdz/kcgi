74f95d783e5cbd7f55c7be5073ac0e41c6b3fd90
kristapsdz <kristaps@bsd.lv>
2020-05-06 22:08:49 +0000

    Have the date2epoch check use native time for its tests more intelligently to avoid casting between int64_t and time_t.

--- regress/test-date2epoch.c
+++ regress/test-date2epoch.c
@@ -38,62 +38,76 @@ main(int argc, char *argv[])
 {
 	size_t	 	 i;
 	struct tm	 test, ntest;
-	time_t		 vv;
-	int64_t		 v, res;
+	time_t		 vv, v;
+	int64_t		 res;
 
 	/* 
-	 * Test a lot of positive and negative numbers.
-	 * We want the full range of time_t to work.
+	 * This is a wanky test, but test-datetime2epoch uses a much
+	 * fuller approach, so this is mostly instructional.
+	 * To create a date we can check, create a random time, break it
+	 * down, set its time to zero, re-create the time from that,
+	 * then check against it.
+	 * We use the native time_t for this, so we're going to be
+	 * restricted to the operating system's time_t width.
+	 * For Solaris, for example, this is limited.
 	 */
 
 	for (i = 0; i < 100000; i++) {
 #if HAVE_ARC4RANDOM
-		v = arc4random();
+		v = (time_t)arc4random();
 #else
-		v = random();
+		v = (time_t)random();
 #endif
 		if (!KHTTP_EPOCH2TM(v, &test))
 			errx(1, "KHTTP_EPOCH2TM");
+
 		memset(&ntest, 0, sizeof(struct tm));
 		ntest.tm_mday = test.tm_mday;
 		ntest.tm_mon = test.tm_mon;
 		ntest.tm_year = test.tm_year;
-		vv = timegm(&ntest);
+		if ((vv = timegm(&ntest)) == -1)
+			errx(1, "timegm");
 
 		if (!khttp_date2epoch(&res,
 		    test.tm_mday,
 		    test.tm_mon + 1,
 		    test.tm_year + 1900))
-			errx(1, "khttp_date2epoch: %" PRId64, vv);
+			errx(1, "khttp_date2epoch: %lld", 
+				(long long)vv);
 
 		if (res != vv) 
 			errx(1, "date cross-check: have "
-				"%" PRId64 ", want %" PRId64, res, vv);
+				"%" PRId64 ", want %lld", res, 
+				(long long)vv);
 	}
 
 	for (i = 0; i < 100000; i++) {
 #if HAVE_ARC4RANDOM
-		v = -arc4random();
+		v = (time_t)arc4random() * -1;
 #else
-		v = -random();
+		v = (time_t)random() * -1;
 #endif
 		if (!KHTTP_EPOCH2TM(v, &test))
 			errx(1, "KHTTP_EPOCH2TM");
+
 		memset(&ntest, 0, sizeof(struct tm));
 		ntest.tm_mday = test.tm_mday;
 		ntest.tm_mon = test.tm_mon;
 		ntest.tm_year = test.tm_year;
-		vv = timegm(&ntest);
+		if ((vv = timegm(&ntest)) == -1)
+			errx(1, "timegm");
 
 		if (!khttp_date2epoch(&res,
 		    test.tm_mday,
 		    test.tm_mon + 1,
 		    test.tm_year + 1900))
-			errx(1, "khttp_date2epoch: %" PRId64, vv);
+			errx(1, "khttp_date2epoch: %lld",
+				(long long)vv);
 
 		if (res != vv) 
 			errx(1, "date cross-check: have "
-				"%" PRId64 ", want %" PRId64, res, vv);
+				"%" PRId64 ", want %lld", res, 
+				(long long)vv);
 	}
 
 	/* Leap year is not ok. */
