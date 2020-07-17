c46647f70ae8dd7583055698180b4b085f5534e7
kristapsdz <kristaps@bsd.lv>
2020-07-17 07:56:42 +0000

    Remove XWARN and use style(9).

--- sandbox-capsicum.c
+++ sandbox-capsicum.c
@@ -34,74 +34,76 @@
 static int
 ksandbox_capsicum_init_control(int worker, int fdfiled, int fdaccept)
 {
-	int rc;
+	int		 rc;
 	struct rlimit	 rl_zero;
 	cap_rights_t	 rights;
 
 	cap_rights_init(&rights);
 
-	if (-1 != fdaccept) {
-		/*
-		 * If we have old-style accept FastCGI sockets, then
-		 * mark us as accepting on it.
-		 * XXX: the CAP_READ and CAP_WRITE are necessary 
-		 * because they're required by descriptors we create
-		 * from the accept().
-		 */
+	/*
+	 * If we have old-style accept FastCGI sockets, then mark us as
+	 * accepting on it.
+	 * XXX: the CAP_READ and CAP_WRITE are necessary because they're
+	 * required by descriptors we create from the accept().
+	 * The new-style passing of descriptors is easier.
+	 */
+
+	if (fdaccept != -1) {
 		cap_rights_init(&rights, 
 			CAP_EVENT, CAP_FCNTL, CAP_ACCEPT, 
 			CAP_READ, CAP_WRITE);
 		if (cap_rights_limit(fdaccept, &rights) < 0 && 
 			 errno != ENOSYS) {
-			XWARN("cap_rights_limit: accept socket");
-			return(0);
+			kutil_warn(NULL, NULL, "cap_rights_limit");
+			return 0;
 		}
 	} else {
-		/* New-style descriptor-passing socket. */
-		assert(-1 != fdfiled);
+		assert(fdfiled != -1);
 		cap_rights_init(&rights, CAP_EVENT, 
 			CAP_FCNTL, CAP_READ, CAP_WRITE);
 		if (cap_rights_limit(fdfiled, &rights) < 0 && 
 			 errno != ENOSYS) {
-			XWARN("cap_rights_limit: descriptor socket");
-			return(0);
+			kutil_warn(NULL, NULL, "cap_rights_limit");
+			return 0;
 		}
 	}
 
 	/* Always pass through write-only stderr. */
+
 	cap_rights_init(&rights, CAP_WRITE, CAP_FSTAT);
 	if (cap_rights_limit(STDERR_FILENO, &rights) < 0 && 
 		 errno != ENOSYS) {
-		XWARN("cap_rights_limit: STDERR_FILENO");
-		return(0);
+		kutil_warn(NULL, NULL, "cap_rights_limit");
+		return 0;
 	}
 
 	/* Interface to worker. */
+
 	cap_rights_init(&rights, CAP_EVENT, 
 		CAP_FCNTL, CAP_READ, CAP_WRITE);
 	if (cap_rights_limit(worker, &rights) < 0 && 
 		 errno != ENOSYS) {
-		XWARN("cap_rights_limit: internal socket");
-		return(0);
+		kutil_warn(NULL, NULL, "cap_rights_limit");
+		return 0;
 	}
 
 	rl_zero.rlim_cur = rl_zero.rlim_max = 0;
-	if (-1 == setrlimit(RLIMIT_FSIZE, &rl_zero)) {
-		XWARNX("setrlimit: rlimit_fsize");
-		return(0);
-	} else if (-1 == setrlimit(RLIMIT_NPROC, &rl_zero)) {
-		XWARNX("setrlimit: rlimit_nproc");
-		return(0);
+	if (setrlimit(RLIMIT_FSIZE, &rl_zero) == -1) {
+		kutil_warn(NULL, NULL, "setrlimit");
+		return 0;
+	} else if (setrlimit(RLIMIT_NPROC, &rl_zero) == -1) {
+		kutil_warn(NULL, NULL, "setrlimit");
+		return 0;
 	}
 
 	rc = cap_enter();
-	if (0 != rc && errno != ENOSYS) {
-		XWARN("cap_enter");
+	if (rc && errno != ENOSYS) {
+		kutil_warn(NULL, NULL, "cap_enter");
 		rc = 0;
 	} else
 		rc = 1;
 
-	return(rc);
+	return rc;
 }
 
 static int
@@ -117,53 +119,56 @@ ksandbox_capsicum_init_worker(int fd1, int fd2)
 	 * Test for EBADF because STDIN_FILENO is usually closed by the
 	 * caller.
 	 */
+
 	cap_rights_init(&rights, CAP_EVENT, CAP_READ, CAP_FSTAT);
 	if (cap_rights_limit(STDIN_FILENO, &rights) < 0 && 
 		 errno != ENOSYS && errno != EBADF) {
- 		XWARN("cap_rights_limit: STDIN_FILENO");
-		return(0);
+ 		kutil_warn(NULL, NULL, "cap_rights_limit");
+		return 0;
 	}
 
 	cap_rights_init(&rights, CAP_EVENT, CAP_WRITE, CAP_FSTAT);
 	if (cap_rights_limit(STDERR_FILENO, &rights) < 0 && 
 		 errno != ENOSYS) {
-		XWARN("cap_rights_limit: STDERR_FILENO");
-		return(0);
+		kutil_warn(NULL, NULL, "cap_rights_limit");
+		return 0;
 	}
 
+	/* Only do thesee if the descriptors are valid. */
+
 	cap_rights_init(&rights, CAP_EVENT, CAP_READ, CAP_WRITE, CAP_FSTAT);
-	if (-1 != fd1 && cap_rights_limit(fd1, &rights) < 0 && 
+	if (fd1 != -1 && cap_rights_limit(fd1, &rights) < 0 && 
 		 errno != ENOSYS) {
-		XWARN("cap_rights_limit: internal socket");
-		return(0);
+		kutil_warn(NULL, NULL, "cap_rights_limit");
+		return 0;
 	}
-	if (-1 != fd2 && cap_rights_limit(fd2, &rights) < 0 && 
+	if (fd2 != -1 && cap_rights_limit(fd2, &rights) < 0 && 
 		 errno != ENOSYS) {
-		XWARN("cap_rights_limit: internal socket");
-		return(0);
+		kutil_warn(NULL, NULL, "cap_rights_limit");
+		return 0;
 	}
 
 	rl_zero.rlim_cur = rl_zero.rlim_max = 0;
 
-	if (-1 == setrlimit(RLIMIT_NOFILE, &rl_zero)) {
-		XWARNX("setrlimit: rlimit_fsize");
-		return(0);
-	} else if (-1 == setrlimit(RLIMIT_FSIZE, &rl_zero)) {
-		XWARNX("setrlimit: rlimit_fsize");
-		return(0);
-	} else if (-1 == setrlimit(RLIMIT_NPROC, &rl_zero)) {
-		XWARNX("setrlimit: rlimit_nproc");
-		return(0);
+	if (setrlimit(RLIMIT_NOFILE, &rl_zero) == -1) {
+		kutil_warn(NULL, NULL, "setrlimit");
+		return 0;
+	} else if (setrlimit(RLIMIT_FSIZE, &rl_zero) == -1) {
+		kutil_warn(NULL, NULL, "setrlimit");
+		return 0;
+	} else if (setrlimit(RLIMIT_NPROC, &rl_zero) == -1) {
+		kutil_warn(NULL, NULL, "setrlimit");
+		return 0;
 	}
 
 	rc = cap_enter();
-	if (0 != rc && errno != ENOSYS) {
-		XWARN("cap_enter");
+	if (rc && errno != ENOSYS) {
+		kutil_warn(NULL, NULL, "cap_enter");
 		rc = 0;
 	} else
 		rc = 1;
 
-	return(rc);
+	return rc;
 }
 
 int
@@ -173,16 +178,16 @@ ksandbox_capsicum_init_child(enum sandtype type,
 	int	 rc;
 
 	switch (type) {
-	case (SAND_WORKER):
+	case SAND_WORKER:
 		rc = ksandbox_capsicum_init_worker(fd1, fd2);
 		break;
-	case (SAND_CONTROL_OLD):
-		assert(-1 == fd2);
+	case SAND_CONTROL_OLD:
+		assert(fd2 == -1);
 		rc = ksandbox_capsicum_init_control
 			(fd1, fdfiled, fdaccept);
 		break;
-	case (SAND_CONTROL_NEW):
-		assert(-1 == fd2);
+	case SAND_CONTROL_NEW:
+		assert(fd2 == -1);
 		rc = ksandbox_capsicum_init_control
 			(fd1, fdfiled, fdaccept);
 		break;
@@ -190,9 +195,10 @@ ksandbox_capsicum_init_child(enum sandtype type,
 		abort();
 	}
 
-	if ( ! rc) 
-		XWARNX("capsicum sandbox failure");
-	return(rc);
+	if (!rc) 
+		kutil_warnx(NULL, NULL, "capsicum sandbox failure");
+
+	return rc;
 }
 
 #else
