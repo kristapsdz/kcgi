d184086ee7bab6b968be6cf30a6c57f13f63dd7b
kristapsdz <kristaps@bsd.lv>
2020-07-21 12:58:42 +0000

    Copyright.

--- child.c
+++ child.c
@@ -1,6 +1,6 @@
 /*	$Id$ */
 /*
- * Copyright (c) 2012, 2014--2016, 2018 Kristaps Dzonsons <kristaps@bsd.lv>
+ * Copyright (c) 2012, 2014--2020 Kristaps Dzonsons <kristaps@bsd.lv>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
